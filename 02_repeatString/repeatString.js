3a072d8c0415d402ddc31ac6e9842c25eeafa1fe
priyanka-gpta <61194307+priyanka-gpta@users.noreply.github.com>
2025-03-13 10:23:19 +0530

    completing 01 and 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(words, repeat) {
+    if (repeat <0) {
+        return "ERROR";
+    }
+    let string = "";
+    for (let i=0;i<repeat;i++) {
+        string = string + words;
+    }
+    return string;
 };
 
 // Do not edit below this line
